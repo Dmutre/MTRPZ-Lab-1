680110d8aee7064320d167d4a3976cd4db5ff584
Dmutre <dmytro.lesko@gmail.com>
2024-02-28 18:06:27 +0100

    Протестував доступ і правильне зчитування файлів
    Додав функції, що зчитують дані з файлів, додав try catch обгортку, щоб функції у разі помилки коректно виводили дані помилки, а також щоб сам код головної функції виглядав більш зрозумілим

--- index.js
+++ index.js
@@ -4,3 +4,27 @@ const path = require('path');
 const args = process.argv.slice(2);
 const inputFilePath = args[0];
 const outputFilePath = args.includes('--out') ? args[args.indexOf('--out') + 1] : null;
+
+function tryCatchFunction(handle) {
+  try {
+    handle();
+  } catch(error) {
+    console.log(error.message);
+    process.exit(0);
+  }
+}
+
+function main() {
+  const dataFromInputFile = readFile();
+  console.log(dataFromInputFile);
+}
+
+function readFile() {
+  return fs.readFileSync(getPathFromDir(inputFilePath), {encoding: 'utf8'});
+}
+
+function getPathFromDir(filePath) {
+  return path.join(__dirname, filePath);
+}
+
+tryCatchFunction(main);
